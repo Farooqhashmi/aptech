5642b352a89fa6d0ddb124d4c7ad4fb100329630
Farooqhashmi <168897828+Farooqhashmi@users.noreply.github.com>
2024-05-06 16:02:21 +0500

    Update addcategory.php

--- scss/addcategory.php
+++ scss/addcategory.php
@@ -3,35 +3,35 @@
         ?>
 
  <!-- Blank Start -->
- <div class="container-fluid pt-4 px-4">
-                <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
-                    <div class="col-md-6 text-center">
-                        <h3>This is blank page</h3>
+            <div class="container-fluid pt-4 px-4">
+                <div class="row vh-100 bg-light rounded  mx-0">
+                    <div class="col-md-12">
+                    <div class="bg-light rounded h-100 p-4">
+                            <h6 class="mb-4">add category</h6>
+                            <form>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label">categorytype</label>
+                                    <input type="text" class="form-control" name="cName" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp">
+                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">image</label>
+                                    <input type="file" name="cImage" class="form-control" id="exampleInputPassword1">
+                                </div>
+                                <!-- <div class="mb-3 form-check">
+                                    <input type="file" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                </div> -->
+                                <button type="submit" name="add category">addcategory</button>
+                            </form>
+                        </div>
+
                     </div>
                 </div>
             </div>
             <!-- Blank End -->
-
-            <div class="bg-light rounded h-100 p-4">
-    <h6 class="mb-4">Add Category</h6>
-    <form action="add_category.php" method="POST" enctype="multipart/form-data">
-        <div class="mb-3">
-            <label for="categoryName" class="form-label">Category Name</label>
-            <input type="text" class="form-control" id="categoryName" name="categoryName" required>
-        </div>
-        <div class="mb-3">
-            <label for="categoryDescription" class="form-label">Category Description</label>
-            <textarea class="form-control" id="categoryDescription" name="categoryDescription" rows="3"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="categoryImage" class="form-label">Category Image</label>
-            <input type="file" class="form-control" id="categoryImage" name="categoryImage">
-        </div>
-        <!-- You can add more fields here if needed -->
-
-        <button type="submit" class="btn btn-primary">Add Category</button>
-    </form>
-</div>
 
 <?php
     @include("components/footer.php");
