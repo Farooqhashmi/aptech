42b5e859d51f0f3a9bf3c53ccd8c131fa49ba182
Farooqhashmi <168897828+Farooqhashmi@users.noreply.github.com>
2024-05-09 21:01:04 +0500

    Update dbcon.php

--- php/dbcon.php
+++ php/dbcon.php
@@ -4,10 +4,3 @@ $user = "root";
 $password = "";
 $pdo = new PDO($server, $user, $password);
 ?>
-
-<?php 
-$server = "mysql:host=localhost;dbname=navttc3db";
-$user = "root";
-$password = "";
-$pdo = new PDO($server, $user, $password);
-?>
