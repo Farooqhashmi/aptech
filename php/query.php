9e99fde49347fa61f3bf0ed9f8cc15ad3c38c95c
Farooqhashmi <hashmifarooq4@gmail.com>
2024-05-12 17:10:07 +0500

    dashmin final

--- php/query.php
+++ php/query.php
@@ -1,122 +1,152 @@
 <?php
 include("dbcon.php");
 //category reference
-$categoryref="img/category/";
-$productref="img/product/";
-if (isset($_POST["addcategory"])){
+$categoryref = "img/category/";
+$productref = "img/product/";
+
+if (isset($_POST["addcategory"])) {
     $catName = $_POST["catName"];
     $catImageName = $_FILES['catImage']['name'];
-    $catTmpName =$_FILES['catImage']['tmp_name'];
-    $extension =pathinfo($catImageName,PATHINFO_EXTENSION);
-    $desig = "img/category/".$catImageName;
-    if($extension=="jpg"||$extension =="jpeg"|| $extension =="png" ||$extension== "webp")
-    {
+    $catTmpName = $_FILES['catImage']['tmp_name'];
+    $extension = pathinfo($catImageName, PATHINFO_EXTENSION);
+    $desig = "img/category/" . $catImageName;
 
-        if(move_uploaded_file($catTmpName,$desig)){
-            $query = $pdo->prepare ("INSERT INTO `categories`( `catName`, `catImage`) 
-            VALUES (:pName,:pImage)");
-            $query->bindParam("pName",$catName);
-            $query->bindParam("pImage",$catImageName);
+    if ($extension == "jpg" || $extension == "jpeg" || $extension == "png" || $extension == "webp") {
+        if (move_uploaded_file($catTmpName, $desig)) {
+            $query = $pdo->prepare("INSERT INTO `categories`( `catName`, `catImage`) VALUES (:productName,:productImage)");
+            $query->bindParam(":productName", $catName);
+            $query->bindParam(":productImage", $catImageName);
             $query->execute();
-    
-            echo"<script>alert('category added')</script>";
+            echo "<script>alert('category added')</script>";
+        } else {
+            echo "<script>alert('failed to upload image')</script>";
+        }
     } else {
-        echo "<script>alert('failed to upload image')</script>";
-        }
-        
-    }
-    else {
-       echo"<script>alert('invalid file type')</script>";
+        echo "<script>alert('invalid file type')</script>";
     }
 }
-//update category
-if(isset($_POST['editcategory'])){
-    $catid = $_POST['catId'];
-    $catName = $cName['cName'];
-    if(!empty($_FILES['catImage']['name'])){
-    $catImageName = $_FILES['cImage']['name'];
-$catTmpname = $_FILES['catImage']['tmp_name'];
-$extension = pathinfo($catImageName,PATHINFO_EXTENSION);
-$desig = "img/category/".$catImageName;
-if($extension =="jpg" || $extension =="png" || $extension == "jpeg" || $extension =="webp"){
-    if(move_uploaded_file($catTmpname,$desig)){
-        $query =$pdo->prepare("UPDATE categories set catName = :pname , catImage = :pimage WHERE catId = :pid");
-        $query->bindParam("pid",$catId);
-        $query->bindParam("pname",$catName);
-        $query->bindParam("pimage",$catImageName);
-        $query->execute();
-        echo "<script>alert('Category Updated')
-        location.assign('viewcategory.php')
-        </script>";
-        }else
-        {
-            echo "<script>alert('fail')</script>";
-        
-        }
-    }
-}
-else{
-    $query =$pdo->prepare("UPDATE categories set catname = :pname WHERE catid = :pid");
-    $query->bindParam("pid",$catid);
-    $query->bindParam("pname",$catName);
-    $query->execute();
-    echo "<script>alert('Category Updated without Image')
-    location.assign(viewcategory.php)
-    </script>";
 
+//update category
+if (isset($_POST['editcategory'])) {
+    $catId = $_POST['catId'];
+    $catName = $_POST['catName'];
+
+    if (!empty($_FILES['catImage']['name'])) {
+        $catImageName = $_FILES['catImage']['name'];
+        $catTmpname = $_FILES['catImage']['tmp_name'];
+        $extension = pathinfo($catImageName, PATHINFO_EXTENSION);
+        $desig = "img/category/" . $catImageName;
+
+        if ($extension == "jpg" || $extension == "png" || $extension == "jpeg" || $extension == "webp") {
+            if (move_uploaded_file($catTmpname, $desig)) {
+                $query = $pdo->prepare("UPDATE categories set catName = :productName , catImage = :productImage WHERE catId = :pid");
+                $query->bindParam(":pid", $catId);
+                $query->bindParam(":productName", $catName);
+                $query->bindParam(":productImage", $catImageName);
+                $query->execute();
+                echo "<script>alert('Category Updated'); location.assign('viewcategory.php');</script>";
+            } else {
+                echo "<script>alert('fail')</script>";
+            }
+        }
+    } else {
+        $query = $pdo->prepare("UPDATE categories set catName = :pname WHERE catId = :pid");
+        $query->bindParam(":pid", $catId);
+        $query->bindParam(":pname", $catName);
+        $query->execute();
+        echo "<script>alert('Category Updated without Image'); location.assign('viewcategory.php');</script>";
+    }
 }
-  }
 
 //delete category
-if(isset($_GET['deleteKey'])){
+if (isset($_GET['deleteKey'])) {
     $catId = $_GET['deleteKey'];
-    $query= $pdo->prepare("DELETE FROM categories Where catId = :pid");
-    $query->bindParam("pid", $catId);
+    $query = $pdo->prepare("DELETE FROM categories Where catId = :pid");
+    $query->bindParam(":pid", $catId);
     $query->execute();
-    echo "<script>alert('Category Deleted');
-    location.assign('viewcategory.php')
-    </script>";
+    echo "<script>alert('Category Deleted'); location.assign('viewcategory.php');</script>";
 }
-//Add product
-if(isset($_POST['addproduct'])){
-$productName = $_POST['pName'];
-$productPrice = $_POST['pPrice'];
-$productDescription = $_POST['pDescription'];
-$productQuantity = $_POST['pQuantity'];
-$productCatid = $_POST['pCatid'];
-$productImageName = $_FILES['pImage']["name"];
-$productTmpName = $_FILES['pImage']["tmp_name"];
-$extension = pathinfo($productImageName,PATHINFO_EXTENSION);
-$desig = "img/product/".$productImageName;
-if($extension =="jpg" || $extension =="png" || $extension == "jpeg" || $extension =="webp") {
-    if(move_uploaded_file($productTmpName,$desig)){
-        $query = $pdo->prepare("INSERT INTO products(productname, productquantity, productprice, productdescription, productImage, productcatid) VALUES(:pn,:pq,:pp,:pd,:pi,:pc)");
-        $query->bindParam("pn", $productName);
-        $query->bindParam("pq", $productQuantity);
-        $query->bindParam("pp", $productPrice);
-        $query->bindParam("pd", $productDescription);
-        $query->bindParam("pi", $productImageName);
-        $query->bindParam("pc", $productCatid);
-        $query->execute();
-        echo "<script>alert('product added successfully')
-        location.assign(viewproducts.php)</script>";
 
-    }else
-    {
-        echo "<script>alert('invalid file type')</script>";
-    
+//Add product
+if (isset($_POST['addproduct'])) {
+    $productName = $_POST['productName'];
+    $productPrice = $_POST['productPrice'];
+    $productDescription = $_POST['productDescription'];
+    $productQuantity = $_POST['productQuantity'];
+    $productCatid = $_POST['pCatid'];
+    $productImageName = $_FILES['pImage']["name"];
+    $productTmpName = $_FILES['pImage']["tmp_name"];
+    $extension = pathinfo($productImageName, PATHINFO_EXTENSION);
+    $desig = "img/product/" . $productImageName;
+
+    if ($extension == "jpg" || $extension == "png" || $extension == "jpeg" || $extension == "webp") {
+        if (move_uploaded_file($productTmpName, $desig)) {
+            $query = $pdo->prepare("INSERT INTO products(productname, productquantity, productprice, productdescription, productImage, productcatid) VALUES(:pn,:pq,:pp,:pd,:pi,:pc)");
+            $query->bindParam(":pn", $productName);
+            $query->bindParam(":pq", $productQuantity);
+            $query->bindParam(":pp", $productPrice);
+            $query->bindParam(":pd", $productDescription);
+            $query->bindParam(":pi", $productImageName);
+            $query->bindParam(":pc", $productCatid);
+            $query->execute();
+            echo "<script>alert('product added successfully'); location.assign('viewproduct.php');</script>";
+        } else {
+            echo "<script>alert('invalid file type')</script>";
+        }
     }
 }
-}
+
 //delete product
-if(isset($_GET['prodeleteKey'])){
+if (isset($_GET['prodeleteKey'])) {
     $proid = $_GET['prodeleteKey'];
-    $query= $pdo->prepare("DELETE FROM products Where productid = :pid");
-    $query->bindParam("pid", $proid);
+    $query = $pdo->prepare("DELETE FROM products Where productid = :pid");
+    $query->bindParam(":pid", $proid);
     $query->execute();
-    echo "<script>alert('Product Deleted');
-    location.assign('viewproducts.php')
-    </script>";
-    
+    echo "<script>alert('Product Deleted'); location.assign('viewproduct.php');</script>";
+}
+
+// update product
+if (isset($_POST['updateproduct'])) {
+    $productId = $_POST['proId'];
+    $productName = $_POST['productName'];
+    $productPrice = $_POST['productPrice'];
+    $productDescription = $_POST['productDescription'];
+    $productQuantity = $_POST['productQuantity'];
+    $productCatid = $_POST['pCatid'];
+
+    if (!empty($_FILES['pImage']['name'])) {
+        $productImageName = $_FILES['pImage']["name"];
+        $productTmpName = $_FILES['pImage']["tmp_name"];
+        $extension = pathinfo($productImageName, PATHINFO_EXTENSION);
+        $desig = "img/products/" . $productImageName;
+
+        if ($extension == "jpg" || $extension == "png" || $extension == "jpeg" || $extension == "webp") {
+            if (move_uploaded_file($productTmpName, $desig)) {
+                $query = $pdo->prepare("UPDATE `products` SET `productName`=:pn, `productQuantity`=:pq, `productPrice`=:pp, `productDescription`=:pd, `productcatId`=:pc, `productImage`=:pi WHERE `productId`=:pid");
+                $query->bindParam(":pn", $productName);
+                $query->bindParam(":pq", $productQuantity);
+                $query->bindParam(":pp", $productPrice);
+                $query->bindParam(":pd", $productDescription);
+                $query->bindParam(":pc", $productCatid);
+                $query->bindParam(":pi", $productImageName);
+                $query->bindParam(":pid", $productId);
+                $query->execute();
+                echo "<script>alert('Product updated')</script>";
+            } else {
+                echo "<script>alert('File not uploaded')</script>";
+            }
+        }
+    } else {
+        $query = $pdo->prepare("UPDATE `products` SET `productName`=:pn, `productQuantity`=:pq, `productPrice`=:pp, `productDescription`=:pd, `productcatId`=:pc WHERE `productId`=:pid");
+        $query->bindParam(":pn", $productName);
+        $query->bindParam(":pq", $productQuantity);
+        $query->bindParam(":pp", $productPrice);
+        $query->bindParam(":pd", $productDescription);
+        $query->bindParam(":pc", $productCatid);
+        $query->bindParam(":pid", $productId);
+        $query->execute();
+        echo "<script>alert('Product updated')</script>";
     }
+}
+
 ?>
--- viewcategory.php
+++ viewcategory.php
@@ -4,7 +4,7 @@ include("components/header.php");
 <!-- Blank Start -->
 <div class="container-fluid pt-4 px-4">
     <div class="row bg-light rounded  mx-0">
-        <div class="col-md">
+        <div class="col-md-12"> <!-- Added "12" to specify column size -->
             <!-- Blank End -->
             <h6 class="mb-4">All Categories</h6>
             <div class="table-responsive">
