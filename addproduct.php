a522a2245d5045913d33d1d0c00e63321fc597c9
SUFIYANSUFIYAN <Sufiyan876@gmail.com>
2024-05-13 02:13:37 +0500

    dashmin product and category completed

---
+++ addproduct.php
@@ -0,0 +1,67 @@
+<?php
+include ("components/header.php");
+?>
+    <!-- Blank Start -->
+    <div class="container-fluid pt-4 px-4">
+        <div class="row bg-light rounded mx-0">
+            <div class="col-md-12 ">
+                <div class="bg-light rounded h-100 p-4">
+                    <h6 class="mb-4">Add Product</h6>
+                    <form method="post" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="productName" class="form-label">Product Name</label>
+                            <input type="text" name="productName" class="form-control" id="productName"
+                                aria-describedby="emailHelp">
+                            <div id="emailHelpProductName" class="form-text">
+                            </div>
+                        </div>
+                        <label for="pCatid" class="form-label">Product Category</label>
+                        <div class="form mb-3">
+                            <select class="form-select" name="pCatid" id="pCatid" aria-label="">
+                                <option selected hidden>Select Any</option>
+                                <?php
+                                $query = $pdo->query("SELECT * from categories");
+                                $row = $query->fetchAll(PDO::FETCH_ASSOC);
+                                foreach($row as $values) {
+                                    ?>
+                                    <option value="<?php echo $values['catId'] ?>">
+                                    <?php echo $values['catName'] ?></option>
+                                    <?php
+                                }
+                                ?>   
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="productDescription">Product Description</label>
+                            <textarea class="form-control" id="productDescription" name="productDescription" 
+                            style="height: 150px;"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="productQuantity" class="form-label">Product Quantity</label>
+                            <input type="number" name="productQuantity" class="form-control" 
+                            id="productQuantity" aria-describedby="emailHelp">
+                            <div id="emailHelpProductQuantity" class="form-text">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="productPrice" class="form-label">Product Price</label>
+                            <input type="number" name="productPrice" class="form-control" id="productPrice"
+                                aria-describedby="emailHelp">
+                            <div id="emailHelpProductPrice" class="form-text">
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="productImage" class="form-label">Image</label>
+                            <input type="file" name="productImage" class="form-control" id="productImage">
+                        </div>
+                        <button type="submit" name="addproduct" class="btn btn-primary">Add Product</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Blank End -->
+
+<?php
+include ("components/footer.php");
+?>
---
+++ updatecategory.php
@@ -0,0 +1,43 @@
+<?php
+include("components/header.php");
+if(isset($_GET['Cid'])){
+    $catstringid = $_GET['Cid'];
+    $query = $pdo->prepare("SELECT * FROM categories WHERE catId=:pid");
+    $query-> bindParam("pid",$catstringid);
+    $query->execute();
+    $catData = $query->fetch(PDO::FETCH_ASSOC);
+}
+?>
+
+<!-- Blank Start -->
+<div class="container-fluid pt-4 px-4">
+    <div class="row vh-100 bg-light rounded mx-0">
+        <div class="col-md-12">
+            <div class="bg-light rounded h-100 p-4">
+                <h6 class="mb-4">Update Category</h6>
+                <form method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Category Name</label>
+                        <input type="text" class="form-control" name="catName" id="exampleInputEmail1"
+                            aria-describedby="emailHelp" value="<?php echo isset($catData['catName']) ? 
+                            $catData['catName'] : ''; ?>">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Image</label>
+                        <input type="file" name="catImage" class="form-control" id="exampleInputPassword1">
+                        <?php if(isset($catData['catImage'])): ?>
+                            <img src="<?php echo $categoryref . $catData['catImage']; ?>" width="80" alt="">
+                        <?php endif; ?>
+                    </div>
+                    <button type="submit" name="editcategory" class="btn btn-primary">Update Category</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Blank End -->
+
+<?php
+include("components/footer.php");
+?>
